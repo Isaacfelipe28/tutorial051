f0f21c56dedf28237a973bda30d702538cfa5d34
ds <ds@labdc.etec>
2026-09-02 20:25:48 -0300

    date e time picker

--- lib/main.dart
+++ lib/main.dart
@@ -39,4 +39,73 @@ class _AgendamentoEventoTelaState extends State<AgendamentosEventoTela> {
   late TimeOfDay _horarioSelecionado;
   
   @override
+  void initstate() {
+    setState((){
+      _dataSelecionada = _datapadrao;
+      _horarioSelecionado = _horarioPadrao;
+    });
+    print('[DEBUG] Formulario resetado para os valores padrão.');
+  }
+  
+  void _salvarFormulario() {
+    print('===============================');
+    print('     Resumo do agendamento     ');
+    print('===============================');
+    print(
+      'Data: ${_dataSelecionada.day}/${_dataSelecionada.month}/${_dataSelecionada.year}'
+    );
+    print('Horário: ${_horarioSelecionado.format(context)}');
+    rint('===============================');
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Evento salvo com sucesso! Veja os log no console.'),
+      ),
+    );
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Novo evento social'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Data e Horario'
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton.icon(
+                    icon: const Icon(Icons.calendar_today),
+                    label: Text(
+                      '${_dataSelecionada.day}/${_dataSelecionada.month}/${_dataSelecionada.year}'
+                    ).
+                    onPressed: () => _selecionarData(context),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: ElavatedButton.icon(
+                    icon: const Icon(Icons.access_time),
+                    label: Text(_horarioSelecionado.format(context)),
+                    onPressed: () => _selecionarHorario(context),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
 }
