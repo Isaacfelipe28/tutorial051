cd3b6047543f063521b9b10ce6f58c50a30cd44f
ds <ds@labdc.etec>
2026-09-09 19:35:26 -0300

    Dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -34,15 +34,18 @@ class _AgendamentoEventoTelaState extends State<AgendamentosEventoTela> {
 
   static final DateTime _datapadrao = DateTime.now();
   static const TimeOfDay _horarioPadrao = TimeOfDay(hour: 19, minute: 0);
+  static const String _tipoPadrao = 'Aniversario';
 
   late DateTime _dataSelecionada;
   late TimeOfDay _horarioSelecionado;
+  late String _tipoEventoSelecionado;
   
   @override
   void initstate() {
     setState((){
       _dataSelecionada = _datapadrao;
       _horarioSelecionado = _horarioPadrao;
+      _tipoEventoSelecionado =_tipoPadrao;
     });
     print('[DEBUG] Formulario resetado para os valores padrão.');
   }
@@ -55,7 +58,8 @@ class _AgendamentoEventoTelaState extends State<AgendamentosEventoTela> {
       'Data: ${_dataSelecionada.day}/${_dataSelecionada.month}/${_dataSelecionada.year}'
     );
     print('Horário: ${_horarioSelecionado.format(context)}');
-    rint('===============================');
+    print('Tipo de evento: $_tipoEventoSelecionado');
+    print('===============================');
 
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(
@@ -102,6 +106,37 @@ class _AgendamentoEventoTelaState extends State<AgendamentosEventoTela> {
                 ),
               ],
             ),
+            const Divider(height: 32),
+            Text(
+              'Tipo de Evento',
+                style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 8),
+            DropdownButtonFormField<String>(
+              initialValue: _tipoEventoSelecionado,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+              ),
+              items: ['Aniversario', 'Casamento', 'Corporativo', 'Outro' ]
+                  .map(
+                    (tipo) => DropdownMenuItem(value: tipo, child: Text(tipo)),
+                  )
+                  toList(),
+              onChanged: (novoValor) {
+                if (novoValor != null) {
+                  setState(() {
+                    _tipoEventoSelecionado = novoValor;
+                  });
+                  print(
+                    '[DEBUG - Menu] Tipo de evento selecionado: $novoValor',
+                  );
+                }
+              },
+            )
             const Divider(height: 32),
           ],
         ),
